0d8d27993040b6bf52ec6a7e08ff7aa8fb309888
PS085822 <Pooja.Sehgal@cerner.com>
2021-09-21 16:28:00 +0530

    error handling

--- app.js
+++ app.js
@@ -14,11 +14,17 @@ function getTranslationalURL(text){
     return url + "?" + "text=" + text
 }
 
+function erroHandler(error){
+    console.log("Error occured!!", error)
+    alert("Something went wrong!! Please try again after some time.")
+}
+
 function clickEventHandler(){
     var input = inputText.value;
     fetch(getTranslationalURL(input))
     .then(response => response.json())
     .then(json => console.log(json.contents.translated))
+    .catch(erroHandler)
 }
 
 btnClick.addEventListener("click", clickEventHandler);
